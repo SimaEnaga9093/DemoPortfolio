cdaba60acdd7d9dd2849e3de9ad1a766f56d426c
SimaEnaga <vharugun9093@gmail.com>
2026-04-21 17:13:18 +0900

    [init] 템플릿 프로젝트 세팅

---
+++ Source/DemoPortfolio/DemoPortfolio.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DemoPortfolio.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DemoPortfolio, "DemoPortfolio" );
+
+DEFINE_LOG_CATEGORY(LogDemoPortfolio)
+ 
---
+++ Source/DemoPortfolio/DemoPortfolioGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DemoPortfolioGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADemoPortfolioGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADemoPortfolioGameMode();
+};
+
+
+
